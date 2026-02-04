ddcab013db16006fa5b61a623a3db3141869135f
Mateus-Santos13 <mateus.santos.vieira.eng.elt@gmail.com>
2026-02-04 10:00:49 -0300

    Criação dos primeiros arquivos do projeto da conta bancária

--- menu.ts
+++ menu.ts
@@ -1,5 +1,5 @@
-import leia from "readline-sync";
-import { colors } from './src/util/Colors';
+import leia from "readline-sync";//import readline sync para leitura de dados do teclado
+import { colors } from './src/util/Colors';//importando o arquivo colors.ts
 
 
 export function main(){
@@ -7,30 +7,37 @@ export function main(){
     let opcao: number;
 
     while(true){
-        //Aqui entra os console log do menu com as configurações de cor
-        console.log(colors.bg.black, colors.fg.magenta);
-        console.log("************************************************************");
-        console.log("\n");
-        console.log("                   Banco Santos Brasil                      ");
-        console.log("\n");
-        console.log("************************************************************");
-        console.log("\n");
-        console.log("1 - Criar Conta");
-        console.log("2 - Listar todas as contas");
-        console.log("3 - Buscar conta por número");
-        console.log("4 - Atualizar dados da conta");
-        console.log("5 - Apagar conta");
-        console.log("6 - Sacar");
-        console.log("7 - Depositar");
-        console.log("8 - Transferir valor entre contas");
-        console.log("9 - Sair");
-        console.log("\n");
-        console.log("************************************************************");
-        console.log("\n");
-        console.log(colors.reset);
-        console.log(colors.fg.red,colors.bg.black);
-        console.log("Escolha uma opção: ");
-        opcao = leia.questionInt();
+    //Aqui entra os console log do menu com as configurações de cor
+    console.log(colors.bg.black, colors.fg.magenta);// Cor de fundo preta e cor da fonte magenta
+    console.log("============================================================");
+    console.log(`
+╔╗ ┌─┐┌┐┌┌─┐┌─┐  ╔═╗┌─┐┌┐┌┌┬┐┌─┐┌─┐  ╔╗ ┬─┐┌─┐┌─┐┬┬
+╠╩╗├─┤││││  │ │  ╚═╗├─┤│││ │ │ │└─┐  ╠╩╗├┬┘├─┤└─┐││
+╚═╝┴ ┴┘└┘└─┘└─┘  ╚═╝┴ ┴┘└┘ ┴ └─┘└─┘  ╚═╝┴└─┴ ┴└─┘┴┴─┘
+`);
+    console.log("============================================================");
+
+    console.log(colors.fg.cyan);
+    console.log("┌────────────────────── MENU ────────────────────────┐");
+    console.log("│                                                    │");
+    console.log("│  1 ─ Criar conta                                   │");
+    console.log("│  2 ─ Listar todas as contas                        │");
+    console.log("│  3 ─ Buscar conta por número                       │");
+    console.log("│  4 ─ Atualizar dados da conta                      │");
+    console.log("│  5 ─ Apagar conta                                  │");
+    console.log("│                                                    │");
+    console.log("│  6 ─ Sacar                                         │");
+    console.log("│  7 ─ Depositar                                     │");
+    console.log("│  8 ─ Transferir valor entre contas                 │");
+    console.log("│                                                    │");
+    console.log("│  9 ─ Sair                                          │");
+    console.log("│                                                    │");
+    console.log("└────────────────────────────────────────────────────┘");
+
+console.log(colors.reset);// Reseta as cores
+console.log(colors.fg.yellow);// Cor da fonte amarela
+console.log("\n➜ Escolha uma opção: ");// Escolha uma opção
+opcao = leia.questionInt();
         
         if(opcao == 9){
             console.log("\nBanco Santos Brasil, o seu banco de confiança!");
@@ -72,7 +79,7 @@ export function main(){
 
 }
 
-export function sobre(): void{
+export function sobre(): void{//Função sobre, apenas mostra os dados da pessoa que desenvolveu (Eu)
     console.log("\n************************************");
     console.log("Projeto desenvolvido por: ");
     console.log("Mateus Santos");
